8ad9dd35b016d78d7c2db7cfa35b443988eefeb7
prashant487 <78904858+prashant487@users.noreply.github.com>
2021-06-03 14:46:05 +0545

    models

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(Categorie)
+admin.site.register(Slider)
+admin.site.register(Ad)
+admin.site.register(Brand)
+admin.site.register(Item)
+
+
+
+
+
+
+
+
+
--- home/models.py
+++ home/models.py
@@ -1,3 +1,56 @@
 from django.db import models
 
+STATUS = (('In Stock', 'In Stock'), ('Out of Stock', 'Out of STOCK'))
+
+
 # Create your models here.
+class Categorie(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.CharField(max_length=200, unique=True)
+    image = models.CharField(max_length=500, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Slider(models.Model):
+    name = models.CharField(max_length=300)
+    image = models.TextField()
+    description = models.TextField()
+    url = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Ad(models.Model):
+    name = models.CharField(max_length=300)
+    rank = models.IntegerField(unique=True)
+    image = models.TextField()
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length=300)
+    image = models.TextField()
+    rank = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Item(models.Model):
+    title = models.CharField(max_length=300)
+    price = models.IntegerField()
+    slug = models.CharField(max_length=300, unique=True)
+    discountedprice = models.IntegerField(default=0)
+    description = models.TextField()
+    category = models.ForeignKey(Categorie, on_delete=models.CASCADE)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    status = models.CharField(max_length=100, choices=STATUS)
+
+    def __str__(self):
+        return self.title
